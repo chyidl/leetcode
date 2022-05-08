9150be0cd6ce17221c441e36ebdc5c6bcad1b6f9
chyidl <chyidl@gmail.com>
2022-05-08 15:15:32 +0800

    update at 2022-05-08

--- 0155-min-stack/min-stack.go
+++ 0155-min-stack/min-stack.go
@@ -4,7 +4,7 @@
 //
 //
 // 	MinStack() initializes the stack object.
-// 	void push(val) pushes the element val onto the stack.
+// 	void push(int val) pushes the element val onto the stack.
 // 	void pop() removes the element on the top of the stack.
 // 	int top() gets the top element of the stack.
 // 	int getMin() retrieves the minimum element in the stack.
